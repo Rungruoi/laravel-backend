43f31b645053bcc8c5a54549605b12cae5e6f121
dungvnph06543 <rungruoiit99@gmail.com>
2019-10-15 16:25:17 +0700

    update commit mrcanh

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,6 +16,7 @@ class ProjectController extends Controller
     public function index()
     {
         $project = $this->projectService->getProject();
+
         return response()->json($project);
     }
 
@@ -23,18 +24,21 @@ class ProjectController extends Controller
     {
         $data = $request->all();
         $addProject = $this->projectService->addProject($data);
+
         return response()->json("success create", 200);
     }
 
     public function update(Request $request, $id)
     {
         $updateProject = $this->projectService->updateProject($id, $request->all());
+
         return response()->json("success update", 200);
     }
 
     public function destroy($id)
     {
         $deleteProject = Project::find($id)->delete();
+
         return response()->json("delete success", 200);
     }
 }
